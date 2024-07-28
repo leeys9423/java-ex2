047337e1c5d2ea79df01dd251280ef9b275f66b3
yslee <leeys9423@gmail.com>
2024-07-28 18:53:30 +0900

    경로 수정

--- src/generic/ex3/AnimalHospitalMainV0.java
+++ src/generic/ex3/AnimalHospitalMainV0.java
@@ -1,14 +1,13 @@
-package generic.test.ex3;
+package generic.ex3;
 
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospitalMainV3 {
+public class AnimalHospitalMainV0 {
 
     public static void main(String[] args) {
-        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
-        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
-        //AnimalHospitalV3<Integer> integerHospital = new AnimalHospitalV3<Integer>(); // 컴파일 오류
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
 
         Dog dog = new Dog("멍멍이1", 100);
         Cat cat = new Cat("야옹이1", 300);
@@ -22,10 +21,8 @@ public class AnimalHospitalMainV3 {
         catHospital.checkup();
 
         // 문제1: 개 병원에 고양이 전달
-        //dogHospital.set(cat); // 다른 타입 입력 컴파일 오
+        //dogHospital.set(cat);
 
-        // 문제2: 개 타입 반환
-        dogHospital.set(dog);
         Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
         System.out.println("biggerDog = " + biggerDog);
     }
