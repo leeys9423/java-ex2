f0a98f0f285263c3a151e6bd19c58f0f06f3937b
yslee <leeys9423@gmail.com>
2024-07-28 16:23:41 +0900

    와일드카드1

--- src/generic/test/ex5/WildcardMain1.java
+++ src/generic/test/ex5/WildcardMain1.java
@@ -1,5 +1,6 @@
 package generic.test.ex5;
 
+import generic.animal.Animal;
 import generic.animal.Cat;
 import generic.animal.Dog;
 
@@ -13,7 +14,12 @@ public class WildcardMain1 {
         dogBox.set(new Dog("멍멍이", 100));
 
         WildcardEx.printGenericV1(dogBox);
+        WildcardEx.printWildcardV1(dogBox);
+
         WildcardEx.printGenericV2(dogBox);
+        WildcardEx.printWildcardV2(dogBox);
+
         Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+        Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
     }
 }
