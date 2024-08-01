0c0fff5f7ebcf41856807ae5e2b3aa0b8efe9ed6
yslee <leeys9423@gmail.com>
2024-08-01 21:36:02 +0900

    리스트 추상화2 - 의존관계 주입

---
+++ src/collection/list/BatchProcessor.java
@@ -0,0 +1,23 @@
+package collection.list;
+
+public class BatchProcessor {
+
+    private final MyList<Integer> list;
+
+    // MyList = new MyArrayList
+    // MyList = new MyLinkedList
+    // 의존 관계 주입
+    public BatchProcessor(MyList<Integer> list) {
+        this.list = list;
+    }
+
+    public void logic(int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(0, i); // 앞에 추가
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+
+    }
+}
